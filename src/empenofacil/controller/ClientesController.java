c1ccf961a5e6366b09240bb5addc305cd5bed7c0
LUNIXINC\lunix <lunix_king@hotmail.com>
2018-10-06 13:40:04 -0500

    Modulo de editar Cliente

--- src/empenofacil/controller/ClientesController.java
+++ src/empenofacil/controller/ClientesController.java
@@ -63,6 +63,7 @@ import mybatis.dao.OcupacionDAO;
  * @author Carlos
  */
 public class ClientesController implements Initializable {
+
     private final ClienteDAO clienteDAO;
     private final DomicilioDAO domicilioDAO;
     private final EstadoDAO estadoDAO;
@@ -75,7 +76,7 @@ public class ClientesController implements Initializable {
     private final ChoiceBox<Estado> estado;
     private final ChoiceBox<Municipio> municipio;
     private final ChoiceBox<Ocupacion> ocupacion;
-    
+
     @FXML
     private VBox form;
     @FXML
@@ -108,79 +109,80 @@ public class ClientesController implements Initializable {
         municipio = new ChoiceBox<>();
         ocupacion = new ChoiceBox<>();
         formulario = Form.of(
-            Section.of(
-                Field.ofStringType(cliente.getNombreProperty())
-                    .label("Nombe")
-                    .validate(RegexValidator.forPattern("(?![\\W])^[\\p{L} .'-]{3,}$", "Introduce un nombre valido."))
-                    .required(true),
-                Field.ofStringType(cliente.getApellidoPaternoProperty())
-                    .label("Apellido paterno")
-                    .span(ColSpan.HALF)
-                    .validate(RegexValidator.forPattern("(?![\\W])^[\\p{L} .'-]{3,}$", "Introduce un apellido paterno valido."))
-                    .required(true),
-                Field.ofStringType(cliente.getApellidoMaternoProperty())
-                    .label("Apellido materno")
-                    .span(ColSpan.HALF)
-                    .validate(RegexValidator.forPattern("^\\.{0}$|(?![\\W])^[\\p{L} .'-]{3,}$", "Introduce un apellido paterno valido.")),
-                Field.ofStringType(cliente.getTelefonoProperty())
-                    .label("Télefono")
-                    .span(ColSpan.HALF)
-                    .validate(RegexValidator.forPattern("^\\.{0}$|\\d{10}", "Introduce un número de telefono de 10 digitos.")),
-                Field.ofStringType(cliente.getCelularProperty())
-                    .label("Celular")
-                    .span(ColSpan.HALF)
-                    .validate(RegexValidator.forPattern("^\\.{0}$|\\d{10}", "Introduce un número de telefono de 10 digitos.")),
-                NodeElement.of(Util.contenedor("Ocupación", ocupacion))
-                    .span(ColSpan.HALF),
-                NodeElement.of(Util.contenedor("Fecha de nacimiento", fechaNacimiento))
-                    .span(ColSpan.HALF),
-                Field.ofStringType(cliente.getCurpProperty())
-                    .label("CURP")
-                    .span(ColSpan.HALF)
-                    .validate(RegexValidator.forPattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{18}$", "Introduce una CURP de 18 caracteres."))
-                    .required(true),
-                Field.ofStringType(cliente.getrfcProperty())
-                    .label("RFC")
-                    .span(ColSpan.HALF)
-                    .validate(RegexValidator.forPattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{13}$", "Introduce el RFC de 13 caracteres."))
-                    .required(true)
-            ).title("Datos personales").collapsible(false),
-            Section.of(
-                Field.ofStringType(domicilio.getCalleProperty())
-                    .label("Calle")
-                    .span(9)
-                    .validate(StringLengthValidator.atLeast(1, "Introduce la calle de tu domicilio."))
-                    .required(true),
-                Field.ofStringType(domicilio.getNumeroProperty())
-                    .label("Número")
-                    .span(3)
-                    .validate(StringLengthValidator.between(1, 5, "Introduce el número de tu domicilio, maximo 5 caracteres."))
-                    .required(true),
-                Field.ofStringType(domicilio.getColoniaProperty())
-                    .label("Colonia")
-                    .span(5)
-                    .validate(StringLengthValidator.atLeast(1, "Introduce la colonia de tu domicilio."))
-                    .required(true),
-                Field.ofStringType(domicilio.getLocalidadProperty())
-                    .label("Localidad")
-                    .span(4)
-                    .validate(StringLengthValidator.atLeast(1, "Introduce la localidad de tu domicilio."))
-                    .required(true),
-                Field.ofIntegerType(domicilio.getCodigoPostalProperty())
-                    .label("Codigo postal")
-                    .span(3)
-                    .validate(IntegerRangeValidator.between(10000, 99999, "Introduce un codigo postal valido de 5 digitos."))
-                    .required(true),
-                NodeElement.of(Util.contenedor("Estado", estado))
-                    .span(ColSpan.HALF),
-                NodeElement.of(Util.contenedor("Municipio", municipio))
-                    .span(ColSpan.HALF)
-            ).title("Domicilio").collapsible(false)
+                Section.of(
+                        Field.ofStringType(cliente.getNombreProperty())
+                                .label("Nombe")
+                                .validate(RegexValidator.forPattern("(?![\\W])^[\\p{L} .'-]{3,}$", "Introduce un nombre valido."))
+                                .required(true),
+                        Field.ofStringType(cliente.getApellidoPaternoProperty())
+                                .label("Apellido paterno")
+                                .span(ColSpan.HALF)
+                                .validate(RegexValidator.forPattern("(?![\\W])^[\\p{L} .'-]{3,}$", "Introduce un apellido paterno valido."))
+                                .required(true),
+                        Field.ofStringType(cliente.getApellidoMaternoProperty())
+                                .label("Apellido materno")
+                                .span(ColSpan.HALF)
+                                .validate(RegexValidator.forPattern("^\\.{0}$|(?![\\W])^[\\p{L} .'-]{3,}$", "Introduce un apellido paterno valido.")),
+                        Field.ofStringType(cliente.getTelefonoProperty())
+                                .label("Télefono")
+                                .span(ColSpan.HALF)
+                                .validate(RegexValidator.forPattern("^\\.{0}$|\\d{10}", "Introduce un número de telefono de 10 digitos.")),
+                        Field.ofStringType(cliente.getCelularProperty())
+                                .label("Celular")
+                                .span(ColSpan.HALF)
+                                .validate(RegexValidator.forPattern("^\\.{0}$|\\d{10}", "Introduce un número de telefono de 10 digitos.")),
+                        NodeElement.of(Util.contenedor("Ocupación", ocupacion))
+                                .span(ColSpan.HALF),
+                        NodeElement.of(Util.contenedor("Fecha de nacimiento", fechaNacimiento))
+                                .span(ColSpan.HALF),
+                        Field.ofStringType(cliente.getCurpProperty())
+                                .label("CURP")
+                                .span(ColSpan.HALF)
+                                .validate(RegexValidator.forPattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{18}$", "Introduce una CURP de 18 caracteres."))
+                                .required(true),
+                        Field.ofStringType(cliente.getrfcProperty())
+                                .label("RFC")
+                                .span(ColSpan.HALF)
+                                .validate(RegexValidator.forPattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{13}$", "Introduce el RFC de 13 caracteres."))
+                                .required(true)
+                ).title("Datos personales").collapsible(false),
+                Section.of(
+                        Field.ofStringType(domicilio.getCalleProperty())
+                                .label("Calle")
+                                .span(9)
+                                .validate(StringLengthValidator.atLeast(1, "Introduce la calle de tu domicilio."))
+                                .required(true),
+                        Field.ofStringType(domicilio.getNumeroProperty())
+                                .label("Número")
+                                .span(3)
+                                .validate(StringLengthValidator.between(1, 5, "Introduce el número de tu domicilio, maximo 5 caracteres."))
+                                .required(true),
+                        Field.ofStringType(domicilio.getColoniaProperty())
+                                .label("Colonia")
+                                .span(5)
+                                .validate(StringLengthValidator.atLeast(1, "Introduce la colonia de tu domicilio."))
+                                .required(true),
+                        Field.ofStringType(domicilio.getLocalidadProperty())
+                                .label("Localidad")
+                                .span(4)
+                                .validate(StringLengthValidator.atLeast(1, "Introduce la localidad de tu domicilio."))
+                                .required(true),
+                        Field.ofIntegerType(domicilio.getCodigoPostalProperty())
+                                .label("Codigo postal")
+                                .span(3)
+                                .validate(IntegerRangeValidator.between(10000, 99999, "Introduce un codigo postal valido de 5 digitos."))
+                                .required(true),
+                        NodeElement.of(Util.contenedor("Estado", estado))
+                                .span(ColSpan.HALF),
+                        NodeElement.of(Util.contenedor("Municipio", municipio))
+                                .span(ColSpan.HALF)
+                ).title("Domicilio").collapsible(false)
         );
     }
 
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
@@ -189,7 +191,7 @@ public class ClientesController implements Initializable {
         clientes.setPlaceholder(new Text("No hay clientes en el sistema..."));
         clientes.getItems().setAll(clienteDAO.obtenerClientes());
         clientes.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue != null) {
+            if (newValue != null) {
                 cargarCliente(newValue);
             }
         });
@@ -209,93 +211,93 @@ public class ClientesController implements Initializable {
         });
         buscar.setText("");
         estado.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue != null) {
+            if (newValue != null) {
                 municipio.setDisable(true);
                 cargarMunicipios(newValue.getIdEstado());
                 municipio.setDisable(false);
             }
         });
     }
-        
+
     private void cargarEstados() {
         municipio.setDisable(true);
         List<Estado> estados = estadoDAO.obtenerEstados();
-        if(estados != null && !estados.isEmpty()) {
+        if (estados != null && !estados.isEmpty()) {
             estado.getItems().setAll(estados);
             municipio.setDisable(false);
         } else {
             Util.dialogo(Alert.AlertType.ERROR, "No hay estados en el sistema");
         }
     }
-    
+
     private void cargarMunicipios(int estado) {
         List<Municipio> municipios = municipioDAO.obtenerMunicipiosPorEstado(estado);
-        if(municipios != null && !municipios.isEmpty()) {
+        if (municipios != null && !municipios.isEmpty()) {
             municipio.getItems().setAll(municipios);
         } else {
             Util.dialogo(Alert.AlertType.ERROR, "No hay municipios en el sistema");
         }
     }
-    
+
     private void cargarOcupaciones() {
         List<Ocupacion> ocupaciones = ocupacionDAO.obtenerOcupaciones();
-        if(ocupaciones != null && !ocupaciones.isEmpty()) {
+        if (ocupaciones != null && !ocupaciones.isEmpty()) {
             ocupacion.getItems().setAll(ocupaciones);
         } else {
             Util.dialogo(Alert.AlertType.ERROR, "No hay ocupaciones en el sistema");
         }
     }
-    
+
     @FXML
     private void nuevo() {
         limpiarFormulario();
         form.setDisable(false);
     }
-    
+
     @FXML
     private void guardar() {
         Ocupacion ocupacionTMP = ocupacion.getValue();
-        if(ocupacionTMP == null) {
+        if (ocupacionTMP == null) {
             Util.dialogo(Alert.AlertType.ERROR, "Elige una ocupación");
             return;
         }
         LocalDate fechaNacimientoTMP = fechaNacimiento.getValue();
-        if(fechaNacimientoTMP == null) {
+        if (fechaNacimientoTMP == null) {
             Util.dialogo(Alert.AlertType.ERROR, "Selecciona una fecha de nacimiento");
             return;
         }
-        if(Period.between(fechaNacimientoTMP, LocalDate.now()).getYears() < 18) {
+        if (Period.between(fechaNacimientoTMP, LocalDate.now()).getYears() < 18) {
             Util.dialogo(Alert.AlertType.ERROR, "Cliente es menor de edad");
             return;
         }
         Estado estadoTMP = estado.getValue();
-        if(estadoTMP == null) {
+        if (estadoTMP == null) {
             Util.dialogo(Alert.AlertType.ERROR, "Elige un estado");
             return;
         }
         Municipio municipioTMP = municipio.getValue();
-        if(municipioTMP == null) {
+        if (municipioTMP == null) {
             Util.dialogo(Alert.AlertType.ERROR, "Elige un municipio");
             return;
         }
-        if(formulario.isValid()) {
+        if (formulario.isValid()) {
             domicilio.setIdMunicipio(municipioTMP.getIdMunicipio());
-            if(domicilioDAO.crearDomicilio(domicilio) == 0) {
+            if (domicilioDAO.crearDomicilio(domicilio) == 0) {
                 Util.dialogo(Alert.AlertType.ERROR, "Ocurrio un error al crear el domicilio del cliente");
                 return;
             }
             cliente.setIdOcupacion(ocupacionTMP.getIdOcupacion());
             cliente.setIdDomicilio(domicilio.getIdDomicilio());
-            if(cliente.getApellidoMaterno().trim().isEmpty()) {
+            if (cliente.getApellidoMaterno().trim().isEmpty()) {
                 cliente.setApellidoMaterno(null);
             }
-            if(cliente.getTelefono().trim().isEmpty()) {
+            if (cliente.getTelefono().trim().isEmpty()) {
                 cliente.setTelefono(null);
             }
-            if(cliente.getCelular().trim().isEmpty()) {
+            if (cliente.getCelular().trim().isEmpty()) {
                 cliente.setCelular(null);
             }
-            if(clienteDAO.crearCliente(cliente) == 0) {
+            if (clienteDAO.crearCliente(cliente) == 0) {
                 Util.dialogo(Alert.AlertType.ERROR, "Ocurrio un error al crear el cliente");
             } else {
                 Util.dialogo(Alert.AlertType.INFORMATION, "Cliente creado correctamente");
@@ -306,22 +308,76 @@ public class ClientesController implements Initializable {
             System.out.println("Corrige los campos antes de continuar");
         }
     }
-    
+
+    public void editarCliente() {
+        Ocupacion ocupacionTMP = ocupacion.getValue();
+        if (ocupacionTMP == null) {
+            Util.dialogo(Alert.AlertType.ERROR, "Elige una ocupación");
+            return;
+        }
+        LocalDate fechaNacimientoTMP = fechaNacimiento.getValue();
+        if (fechaNacimientoTMP == null) {
+            Util.dialogo(Alert.AlertType.ERROR, "Selecciona una fecha de nacimiento");
+            return;
+        }
+        if (Period.between(fechaNacimientoTMP, LocalDate.now()).getYears() < 18) {
+            Util.dialogo(Alert.AlertType.ERROR, "Cliente es menor de edad");
+            return;
+        }
+        Estado estadoTMP = estado.getValue();
+        if (estadoTMP == null) {
+            Util.dialogo(Alert.AlertType.ERROR, "Elige un estado");
+            return;
+        }
+        Municipio municipioTMP = municipio.getValue();
+        if (municipioTMP == null) {
+            Util.dialogo(Alert.AlertType.ERROR, "Elige un municipio");
+            return;
+        }
+        if (formulario.isValid()) {
+            domicilio.setIdMunicipio(municipioTMP.getIdMunicipio());
+            if (domicilioDAO.crearDomicilio(domicilio) == 0) {
+                Util.dialogo(Alert.AlertType.ERROR, "Ocurrio un error al crear el domicilio del cliente");
+                return;
+            }
+            cliente.setIdOcupacion(ocupacionTMP.getIdOcupacion());
+            cliente.setIdDomicilio(domicilio.getIdDomicilio());
+            if (cliente.getApellidoMaterno().trim().isEmpty()) {
+                cliente.setApellidoMaterno(null);
+            }
+            if (cliente.getTelefono().trim().isEmpty()) {
+                cliente.setTelefono(null);
+            }
+            if (cliente.getCelular().trim().isEmpty()) {
+                cliente.setCelular(null);
+            }
+            if (clienteDAO.editarCliente(cliente) == 0) {
+                Util.dialogo(Alert.AlertType.ERROR, "Ocurrio un error al editar el cliente");
+            } else {
+                Util.dialogo(Alert.AlertType.INFORMATION, "Cliente editar correctamente");
+                actualizarClientes();
+                cancelar();
+            }
+        } else {
+            System.out.println("Corrige los campos antes de continuar");
+        }
+    }
+
     @FXML
     private void cancelar() {
         limpiarFormulario();
         form.setDisable(true);
     }
-    
+
     private void actualizarClientes() {
         clientes.getItems().clear();
-        if(buscar.getText().trim().isEmpty()) {
+        if (buscar.getText().trim().isEmpty()) {
             clientes.getItems().setAll(clienteDAO.obtenerClientes());
         } else {
             clientes.getItems().setAll(clienteDAO.buscarClientes(buscar.getText()));
         }
     }
-    
+
     private void limpiarFormulario() {
         ocupacion.getSelectionModel().clearSelection();
         ocupacion.getItems().clear();
@@ -333,11 +389,11 @@ public class ClientesController implements Initializable {
         Domicilio domicilioNuevo = new Domicilio(null, "", "", "", null, "", null);
         cargarCliente(clienteNuevo, domicilioNuevo);
     }
-    
+
     private void cargarCliente(Cliente cliente) {
         cargarCliente(cliente, domicilioDAO.obtenerDomicilio(cliente.getIdDomicilio()));
     }
-    
+
     private void cargarCliente(Cliente cliente, Domicilio domicilio) {
         this.cliente.setIdCliente(cliente.getIdCliente());
         this.cliente.setIdOcupacion(cliente.getIdOcupacion());
@@ -355,21 +411,25 @@ public class ClientesController implements Initializable {
         cargarEstados();
         cargarOcupaciones();
     }
-    
+
     private static class DateFormat extends StringConverter<LocalDate> {
+
         private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         @Override
         public String toString(LocalDate localDate) {
-            if(localDate == null) 
+            if (localDate == null) {
                 return "";
+            }
             return dateTimeFormatter.format(localDate);
         }
+
         @Override
         public LocalDate fromString(String dateString) {
-            if(dateString == null || dateString.trim().isEmpty()) {
+            if (dateString == null || dateString.trim().isEmpty()) {
                 return null;
             }
-            return LocalDate.parse(dateString,dateTimeFormatter);
+            return LocalDate.parse(dateString, dateTimeFormatter);
         }
     }
 }
