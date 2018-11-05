0bd1f8e346f23ae7dcb31be024bd44f65b872ec6
LUNIXINC\lunix <lunix_king@hotmail.com>
2018-11-05 13:38:40 -0600

    Modulo: creacion de gui para ventas

---
+++ src/empenofacil/controller/VentasController.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2018 lunix
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package empenofacil.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author lunix
+ */
+public class VentasController implements Initializable {
+  @FXML
+    private Button agregarab;
+
+    @FXML
+    private Button cncelarb;
+
+    @FXML
+    private TextField ivaf;
+
+    @FXML
+    private Label nombrel;
+
+    @FXML
+    private Label subtotall;
+
+    @FXML
+    private TextField totalf;
+
+    @FXML
+    private TextField direccionf;
+
+    @FXML
+    private Button btnBusqueda1;
+
+    @FXML
+    private Label ival;
+
+    @FXML
+    private TextField acomuladof;
+
+    @FXML
+    private CheckBox notacb;
+
+    @FXML
+    private Label totall;
+
+    @FXML
+    private TextField subtotalf;
+
+    @FXML
+    private Label direccionl;
+
+    @FXML
+    private Label descuentol;
+
+    @FXML
+    private Label lbclave;
+
+    @FXML
+    private TextField clavef;
+
+    @FXML
+    private TextField busquedaTxt;
+
+    @FXML
+    private TableColumn<?, ?> articulotabla;
+
+    @FXML
+    private TextField descuentof;
+
+    @FXML
+    private Label lblAcumlado;
+
+    @FXML
+    private TextField nombref;
+
+    @FXML
+    private Label labelBusqueda;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
